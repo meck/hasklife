14abae36ea1a1a2c59f6b2a7fa9df73eda690c6f
meck <johan@meck.se>
2018-03-09 21:42:07 +0100

    Command line options

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,14 @@
 module Main where
 
 import           Data.List                        (mapAccumL)
+import           Data.List.Split                  (splitOn)
+import           Data.Semigroup                   ((<>))
 import           Graphics.Gloss
 import           Graphics.Gloss.Interface.IO.Game
 import           Logic
+import           Options.Applicative
 
-iniWidth, iniHeight, offset :: Int
-iniWidth = 1000
-
-iniHeight = iniWidth `div` 2
-
-offset = 30
-
-cellsize :: Int
-cellsize = 3
-
-cellsizeF :: Float
-cellsizeF = fromIntegral cellsize
+windowOffset = 30
 
 stepsPerSec :: Int
 stepsPerSec = 8
@@ -32,6 +24,7 @@ textColor = makeColorI 216 222 233 255
 
 helpColor :: Color
 helpColor = makeColorI 235 203 139 255
+
 textScale :: Float
 textScale = 0.2
 
@@ -44,36 +37,24 @@ data World = World
   , paused   :: Bool
   , winW     :: Int
   , winH     :: Int
+  , cellsize :: Int
   , showHelp :: Bool
   }
 
-initialWorld :: World
-initialWorld = World
-  { grid     = rPentomino
-  , itter    = 0
-  , paused   = True
-  , winW     = iniWidth
-  , winH     = iniHeight
-  , showHelp = False
-  }
-
-window :: Display
-window = InWindow "Hasklife" (iniWidth, iniHeight) (offset, offset)
-
-gridToPicture :: Grid -> Picture
-gridToPicture g = pictures $ celltoPict <$> gridToList g
+gridToPicture :: Int -> Grid -> Picture
+gridToPicture cs g = pictures $ celltoPict <$> gridToList g
  where
   celltoPict (x, y) =
-    translate (fromIntegral $ x * cellsize) (fromIntegral $ y * cellsize)
+    translate (fromIntegral $ x * cs) (fromIntegral $ y * cs)
       $ color cellColor
       $ polygon
-      $ rectanglePath cellsizeF cellsizeF
+      $ rectanglePath (fromIntegral cs) (fromIntegral cs)
 
 worldToPicture :: World -> IO Picture
 worldToPicture w = return $ pictures
   [ if showHelp w then helpPicture (winW w) (winH w) else blank
   , itterPicture (winW w) (winH w) (itter w)
-  , gridToPicture $ grid w
+  , gridToPicture (cellsize w) (grid w)
   ]
 
 itterPicture :: Int -> Int -> Int -> Picture
@@ -100,8 +81,9 @@ helpPicture width height =
     $ mapAccumL
         go
         0
-        [ "Space to Pause"
-        , "When Paused:"
+        [ "H: Hide/Show help"
+        , "Space: Unpause/Pause"
+        , "When Paused"
         , " C: clear screen"
         , " R: randomize"
         , " Click: Add or remove Cell"
@@ -134,8 +116,8 @@ handleInput event w = case event of
       if paused w then return $ w { grid = emptyGrid, itter = 0 } else return w
     'r' -> if paused w
       then do
-        let wid = div iniWidth $ 2 * cellsize
-            hig = div iniHeight $ 2 * cellsize
+        let wid = div (winW w) $ 2 * cellsize w
+            hig = div (winH w) $ 2 * cellsize w
         ng <- randomGrid (negate wid, negate hig) (wid, hig)
         return w { grid = ng, itter = 0 }
       else return w
@@ -143,13 +125,70 @@ handleInput event w = case event of
     _   -> return w
   (EventResize (x, y)) -> return $ w { winW = x, winH = y }
   _                    -> return w
-  where scalePix x = truncate $ signum x * (abs x + cellsizeF / 2) / cellsizeF
+ where
+  scalePix x =
+    truncate $ signum x * (abs x + fromIntegral (cellsize w) / 2) / fromIntegral
+      (cellsize w)
+
+data Opt = Opt
+  { wSize :: (Int, Int)
+  , cSize :: Int
+  }
+
+pOpt :: Parser Opt
+pOpt =
+  Opt
+    <$> option
+          sizeReader
+          (  long "size"
+          <> short 's'
+          <> help "Window size"
+          <> showDefault
+          <> value (1000, 500)
+          <> metavar "INT,INT"
+          )
+    <*> option
+          auto
+          (  long "cellsize (Resolution)"
+          <> short 'c'
+          <> help "Size of the cells"
+          <> showDefault
+          <> value 5
+          <> metavar "INT"
+          )
+
+sizeReader :: ReadM (Int, Int)
+sizeReader = do
+  o <- str
+  -- TODO error handling
+  let [w, h] = map read $ splitOn "," o
+  return (w, h)
 
 main :: IO ()
-main = playIO window
-              background
-              stepsPerSec
-              initialWorld
-              worldToPicture
-              handleInput
-              stepWorld
+main = runWorld =<< execParser opts
+ where
+  opts = info
+    (pOpt <**> helper)
+    ( fullDesc <> progDesc "Conways game of life in a OpenGL window" <> header
+      "Haskelife"
+    )
+
+runWorld :: Opt -> IO ()
+runWorld opt = playIO window
+                      background
+                      stepsPerSec
+                      initialWorld
+                      worldToPicture
+                      handleInput
+                      stepWorld
+ where
+  window       = InWindow "Hasklife" (wSize opt) (windowOffset, windowOffset)
+  initialWorld = World
+    { grid     = rPentomino
+    , itter    = 0
+    , paused   = True
+    , winW     = fst $ wSize opt
+    , winH     = snd $ wSize opt
+    , cellsize = cSize opt
+    , showHelp = True
+    }
